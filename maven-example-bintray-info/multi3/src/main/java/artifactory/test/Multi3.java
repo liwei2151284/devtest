113214123f8bbe162aea15072a506f25ee05c26a
liwei2151284 <445169331@qq.com>
2019-02-24 14:48:41 +0800

    Update Multi3.java

--- maven-example-bintray-info/multi3/src/main/java/artifactory/test/Multi3.java
+++ maven-example-bintray-info/multi3/src/main/java/artifactory/test/Multi3.java
@@ -6,6 +6,6 @@ package artifactory.test;
 public class Multi3 {
     public static void main(String[] args) {
         new Multi1();
-        System.out.println("Hello World!");
+        System.out.println("Hello World!sdfs");
     }
 }
